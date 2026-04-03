6540fd91d67a3cb6f63f9cc717f7773d9e22260f
Krunal Rathod <rathod.krunal.rk@gmail.com>
2026-04-03 23:26:59 +0530

    save

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,35 @@
 import React from 'react'
-import React from 'react'
+
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Cart from './pages/Cart'
+import Collection from './pages/Collection'
+import Contact from './pages/Contact'
+import Login from './pages/Login'
+import Order from './pages/Order'
+import Placeorder from './pages/Placeorder'
+import Product from './pages/Product'
+import Navbar from './components/Navbar'
 
 const App = () => {
   return (
-    <div>
+    <div className='px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw]'>
+
+      <Navbar/>
+
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/cart' element={<Cart/>}/>
+        <Route path='/collection' element={<Collection/>}/>
+        <Route path='/contact' element={<Contact/>}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/order' element={<Order/>}/>
+        <Route path='/place-order' element={<Placeorder/>}/>
+        <Route path='/product/:productId' element={<Product/>}/>
+    
+      </Routes>
       
     </div>
   )
