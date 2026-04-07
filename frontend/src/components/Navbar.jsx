8b654c4ca6be166c5ec66ac25019189ab62bff12
Krunal Rathod <rathod.krunal.rk@gmail.com>
2026-04-07 16:15:05 +0530

    save

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -26,18 +26,27 @@ const Navbar = () => {
           </NavLink>
         </ul>
 
-        <div className='flex items-center gap-6'>
-            <img src={assets.search_icon} className='w-5 cursor-pointer'/>
+            <div className='flex items-center gap-6'>
 
-            <div className='group relative'>
-            <img src={assets.profile_icon} className='w-5 cursor-pointer' />
+                <img src={assets.search_icon} className='w-5 cursor-pointer'/>
+                <div className='group relative'>
+                 <img src={assets.profile_icon} className='w-5 cursor-pointer' />
+                    <div className='group-hover:block hidden absolute dropdown-menu right-0 pt-4'>
+                        <div className='flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500 rounded'>
+                            <p className='cursor-pointer hover:text-black'>My Profile</p>
+                            <p className='cursor-pointer hover:text-black'>Order</p>
+                            <p className='cursor-pointer hover:text-black'>Logout</p>
+                        </div>
+                    </div>  
+                </div>
+                <Link to ='/cart' className=''>
+                    <img src={assets.cart_icon} className='w-5 min-w-5' alt='' />
+                    <p className=''></p>
+                </Link>
 
-            
-        </div>
-        </div>
 
-        
-      
+
+            </div>
     </div>
   )
 }
