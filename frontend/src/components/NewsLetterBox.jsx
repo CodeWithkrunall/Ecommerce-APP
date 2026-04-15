0761ba8681b6bc72ff281dbafe5cf112d5e4b994
Krunal Rathod <rathod.krunal.rk@gmail.com>
2026-04-15 14:30:20 +0530

    save

---
+++ frontend/src/components/NewsLetterBox.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const NewsLetterBox = () => {
+  return (
+    <div>
+
+        
+      
+    </div>
+  )
+}
+
+export default NewsLetterBox
--- frontend/src/components/OurPolicy.jsx
+++ frontend/src/components/OurPolicy.jsx
@@ -5,30 +5,23 @@ const OurPolicy = () => {
   return (
     <div className='flex flex-col sm:flex-row justify-around gap-12 sm:gap-2 text-center py-20 text-xs sm:text-sm md:text-base text-gray-700'>
 
-        <div>
-            <img src={assets.exchange_icon} className='w-12 m-auto mb-5' alt="" />
-            <p className='font-semibold'>we exchange policy</p>
-            <p className='text-grey-400'>we offer hassel free exchange policy</p>
+            <div>
+                <img src={assets.exchange_icon} className='w-12 m-auto mb-5' alt="" />
+                <p className='font-semibold'>we exchange policy</p>
+                <p className='text-grey-400'>we offer hassel free exchange policy</p>
+            </div>
 
-        </div>
+            <div>
+                <img src={assets.quality_icon}  className='w-12 m-auto mb-5' alt="" />
+                <p className='font-semibold'>7 days return policy</p>
+                <p className='text-gray-700'>we provide 7 days return policy</p>
+            </div>
 
-        <div>
-            <img src={assets.quality_icon}  className='w-12 m-auto mb-5' alt="" />
-            <p className='font-semibold'>7 days return policy</p>
-            <p className='text-gray-700'>we provide 7 days return policy</p>
-
-        </div>
-
-        <div>
-            <img src={assets.support_img} className='w-12 m-auto mb-5' alt="" />
-            <p className='font-semibold'>we provide full support</p>
-            <p className='text-gray-500'> we provide 24/7 full suppoer</p>
-        </div>
-
-
-
-
-      
+            <div>
+                <img src={assets.support_img} className='w-12 m-auto mb-5' alt="" />
+                <p className='font-semibold'>Best customer suppoer</p>
+                <p className='text-gray-500'> we provide 24/7 customer suppoer</p>
+            </div>
     </div>
   )
 }
