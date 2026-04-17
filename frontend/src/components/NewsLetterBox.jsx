fc7a1c539390c8b3778eba83fb319d97ca3d2dd8
Krunal Rathod <rathod.krunal.rk@gmail.com>
2026-04-17 19:05:21 +0530

    save

--- frontend/src/components/NewsLetterBox.jsx
+++ frontend/src/components/NewsLetterBox.jsx
@@ -3,7 +3,15 @@ import React from 'react'
 const NewsLetterBox = () => {
   return (
     
-    <div>
+    <div className='text-center'>
+      <p className='text-2xl font-medium text-gray-800'>Subscribe now & get 20% off</p>
+      <p className='text-sm text-gray-400 mt-3'>Lorem ipsum dolor sit amet consectetur adipisicing.</p>
+
+      <form className='w-full sm:w-1/2 flex items-center gap-3 mx-auto my-6 border pl-3 '>
+        <input className='w-full sm:flex-1 outline-none' type="email" placeholder='enter your email' required/>
+        <button type='submit' className='bg-black text-white text-xs px-10 py-4'>Subscribe</button>
+      </form>
+
       
     </div>
   )
