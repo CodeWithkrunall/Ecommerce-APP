8fb7db0507767c818af7003c1e4c6c31971ec874
Krunal Rathod <rathod.krunal.rk@gmail.com>
2026-04-18 11:56:02 +0530

    save

--- frontend/src/pages/Collection.jsx
+++ frontend/src/pages/Collection.jsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { useContext, useState } from 'react'
+import { ShopContext } from '../context/ShopContext'
 
 const Collection = () => {
+
+  const {products} = useContext(ShopContext);
+  const[showFilter, setShowFilter] = useState(false)
+  
+  
   return (
-    <div>
-      
+
+    <div className='flex flex-col sm:flex-row gap-1 sm:gap-10 pt-10 border-t'>
+
+      {/* Filters options */}
+      <div className='min-w-60'>
+        <p className='my-2 text-2xl flex items-center cursor-pointer gap-2'>FILTER</p>
+
+        {/* Category Filter */}
+
+          <div className={`border border-gray pl-5 py-3 mt-6 ${'showFilter? " " : "hidden"'}`}>
+
+          </div>
+      </div>
     </div>
   )
 }
