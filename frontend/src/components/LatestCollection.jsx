44ab4aecfcf60cac2b2d3203228c9c85b6308cdf
Krunal Rathod <rathod.krunal.rk@gmail.com>
2026-05-20 21:51:37 +0530

    save

--- frontend/src/components/LatestCollection.jsx
+++ frontend/src/components/LatestCollection.jsx
@@ -9,7 +9,6 @@ const LatestCollection = () => {
     const { products } = useContext(ShopContext)
     const[latestProducts,setLatestProducts] =useState([]);
 
-    
     useEffect(()=>{
         setLatestProducts(products.slice(0,10));
     },[])
